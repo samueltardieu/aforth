976f3be231e784ba89adcb84eda326318ff9814e
Samuel Tardieu <sam@rfc1149.net>
2006-10-12 22:57:23 +0200

    Allow embedding of very long lines with GNAT style checks on

--- embed.py
+++ embed.py
@@ -21,11 +21,23 @@ package body %s is
 begin
 """ % ada)
 
+def split(l):
+    r = ''
+    lines = []
+    while len(l) > 40:
+        lines.append(l[:40])
+        l = l[40:]
+    if l: lines.append(l)
+    for n, l in enumerate(lines):
+        if n: r += ' &\n      '
+        r += '"%s"' % l.replace('"', '""')
+    return r
+
 for l in open(sys.argv[1]):
     while l[-1:] in ["\r", "\n"]: l = l[:-1]
     if not l: continue
-    outbody.write ('   Aforth.Interpret_Line ("%s");\n' %
-                   l.replace('"', '""'))
+    outbody.write ('   Aforth.Interpret_Line (%s);\n' %
+                   split(l))
 
 outbody.write("end %s;\n" % ada)
 
