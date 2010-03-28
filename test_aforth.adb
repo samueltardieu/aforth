36a8834eeb67035e8c1696bad1be5fc03035e8bd
Samuel Tardieu <sam@rfc1149.net>
2010-03-28 17:56:33 +0200

    Allow test_aforth to take file names on the command line

--- test_aforth.adb
+++ test_aforth.adb
@@ -1,10 +1,18 @@
-with Aforth; use Aforth;
-with Aforth.Timing;
-pragma Warnings (Off, Aforth.Timing);
+with Ada.Command_Line;   use Ada.Command_Line;
+with Ada.IO_Exceptions;
+with Aforth;             use Aforth;
 with Aforth.Builtins;
 pragma Warnings (Off, Aforth.Builtins);
+with Aforth.Timing;
+pragma Warnings (Off, Aforth.Timing);
 
 procedure Test_Aforth is
 begin
+   for I in 1 .. Argument_Count loop
+      Include_File (Argument (I));
+   end loop;
    Quit;
+exception
+   when Ada.IO_Exceptions.Name_Error =>
+      Set_Exit_Status (1);
 end Test_Aforth;
